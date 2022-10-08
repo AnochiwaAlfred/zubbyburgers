cf3468a4127168efb80b8ab0639bf633da2287a6
AnochiwaAlfred <42777193+AnochiwaAlfred@users.noreply.github.com>
2022-10-08 22:01:59 +0100

    Update external.js

--- js/external.js
+++ js/external.js
@@ -2,27 +2,27 @@
 document.getElementById("burgers").style="display:block;"
 
 function burgersBtn(){
-	document.getElementById("burgers-btn").className="js-btn .active-btn";
-	document.getElementById("sides-btn").className="js-btn .btn-brws";
-	document.getElementById("drinks-btn").className="js-btn .btn-brws";
+	document.getElementById("burgers-btn").className="active-btn";
+	document.getElementById("sides-btn").className="btn-brws";
+	document.getElementById("drinks-btn").className="btn-brws";
 	document.getElementById("burgers").style="display:block;";
 	document.getElementById("sides").style="display:none; background-color:white;";
 	document.getElementById("drinks").style="display:none; background-color:white;";
 }
 
 function sidesBtn(){
-	document.getElementById("sides-btn").className="js-btn .active-btn";
-	document.getElementById("burgers-btn").className="js-btn .btn-brws";
-	document.getElementById("drinks-btn").className="js-btn .btn-brws";
+	document.getElementById("sides-btn").className="active-btn";
+	document.getElementById("burgers-btn").className="btn-brws";
+	document.getElementById("drinks-btn").className="btn-brws";
 	document.getElementById("sides").style="display:block;";
 	document.getElementById("burgers").style="display:none;";
 	document.getElementById("drinks").style="display:none;";
 }
 
 function drinksBtn(){
-	document.getElementById("drinks-btn").className="js-btn .active-btn";
-	document.getElementById("burgers-btn").className="js-btn .btn-brws";
-	document.getElementById("sides-btn").className="js-btn .btn-brws";
+	document.getElementById("drinks-btn").className="active-btn";
+	document.getElementById("burgers-btn").className="btn-brws";
+	document.getElementById("sides-btn").className="btn-brws";
 	document.getElementById("drinks").style="display:block;";
 	document.getElementById("burgers").style="display:none;";
 	document.getElementById("sides").style="display:none;";
@@ -37,3 +37,21 @@ function navToggleDown(){
 	document.getElementById("nav-links").style="display:none;";
 	document.getElementById("nav-links").onclick="navToggle";
 }
+
+
+
+// const btn = document.querySelectorAll(".js-btn");
+
+// for (let index = 0; index < btn.length; index++) {
+// 	const element = btn[index];
+// 	element.addEventListener("click", function(event){
+// 		if((event.target == this)) {
+// 			element.className="js-btn active-btn"
+// 			// element.style.backgroundColor = "red"
+// 		}else{
+// 			element.className="js-btn"
+// 			element.style.backgroundColor = "white"
+// 		}
+// 	}, false)
+// }
+
